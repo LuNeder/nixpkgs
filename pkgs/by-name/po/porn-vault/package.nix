811b4e3ddbf6fce6e4edc90fad6726b00b68db41
Luana <luana@luana.dev.br>
2026-06-10 18:36:26 -0300

    porn-vault: 0.30.0-rc.11 -> 0.40.0-rc.42

--- pkgs/by-name/po/porn-vault/package.nix
+++ pkgs/by-name/po/porn-vault/package.nix
@@ -2,105 +2,89 @@
   fetchFromGitLab,
   rustPlatform,
   lib,
-  pnpm_10,
+  pnpm_11,
   fetchPnpmDeps,
   pnpmConfigHook,
-  stdenvNoCC,
-  nodejs-slim_22,
+  stdenv,
+  nodejs_26,
+  nodejs-slim_26,
   ffmpeg,
   imagemagick,
+  openssl,
+  pkg-config,
   makeWrapper,
 }:
 let
-  izzy = rustPlatform.buildRustPackage rec {
-    pname = "izzy";
-    version = "2.0.1";
-
-    src = fetchFromGitLab {
-      owner = "porn-vault";
-      repo = "izzy";
-      rev = version;
-      hash = "sha256-UauA5mZi5a5QF7d17pKSzvyaWbeSuFjBrXEAxR3wNkk=";
-    };
-
-    postPatch = ''
-      ln -s ${./Cargo.lock} Cargo.lock
-    '';
-
-    cargoLock.lockFile = ./Cargo.lock;
-
-    meta = {
-      description = "Rust In-Memory K-V Store with Redis-Style File Persistence and Secondary Indices";
-      homepage = "https://gitlab.com/porn-vault/izzy";
-      license = lib.licenses.gpl3Plus;
-      maintainers = [ lib.maintainers.luNeder ];
-      mainProgram = "izzy";
-    };
-  };
-  nodejs-slim = nodejs-slim_22;
-  pnpm' = pnpm_10.override { inherit nodejs-slim; };
+  nodejs = nodejs_26;
+  nodejs-slim = nodejs-slim_26;
+  pnpm' = pnpm_11.override { inherit nodejs-slim; };
 in
-stdenvNoCC.mkDerivation (finalAttrs: {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "porn-vault";
-  version = "0.30.0-rc.11";
+  version = "0.40.0-rc.42";
 
   src = fetchFromGitLab {
     owner = "porn-vault";
     repo = "porn-vault";
-    rev = "4c6182c5825d85193cf67cb7cd927da2feaaecdb";
-    hash = "sha256-wQ3dqLc0l2BmLGDYrbWxX2mPwO/Tqz0fY/fOQTEUv24=";
+    rev = "1597b5646bc4e433b7c0110294b5881de364ce03";
+    hash = "sha256-BvqKqsZNv9flzPx6EbMU2ot/xWX91wQORkhDWaoTJGc=";
   };
+
+  cargoHash = "sha256-S0j3Iojmx5JAGuJejbJuM41YxdZt89zh1k45Qmi7sv4=";
 
   pnpmDeps = fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
+    prePatch = ''
+      cd app
+    '';
     pnpm = pnpm';
     fetcherVersion = 4;
-    hash = "sha256-5ZMa7/3nbG1xsTfd09oh+NTU+FFhd0lV425pU3s9bZE=";
+    hash = "sha256-DX6esVA522VPxJvUaaQuGZ0gBpoJmBt+Cl8Y/OqPxsQ=";
   };
 
   nativeBuildInputs = [
-    nodejs-slim
+    pkg-config
+    openssl
+    nodejs
     pnpmConfigHook
     pnpm'
     makeWrapper
   ];
 
-  patches = [
-    ./allow-use-of-systemd-temp-path.patch
-  ];
+  prePnpmInstall = ''
+    cd app
+  '';
 
   postPatch = ''
-    substituteInPlace server/binaries/izzy.ts \
-      --replace-fail 'chmodSync(izzyPath, "111");' ""
+    substituteInPlace vault/src/temp.rs \
+      --replace-fail 'PV_TMP_FOLDER' "CACHE_DIRECTORY"
   '';
 
-  buildPhase = ''
-    runHook preBuild
-
+  preBuild = ''
     pnpm build
-
-    runHook postBuild
   '';
+
+  PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
+
+  doCheck = false;
 
   installPhase = ''
     runHook preInstall
 
-    install -Dm644 package.json config.example.json remix.config.js -t $out/share/porn-vault
-    cp -R public dist build node_modules graphql locale -t $out/share/porn-vault
+    cd ..
+    mkdir -p $out/bin
+    cp ./target/${stdenv.hostPlatform.config}/release/pv $out/bin/porn-vault-unwrapped
 
     runHook postInstall
   '';
 
   preFixup = ''
-    makeWrapper "${lib.getExe nodejs-slim}" "$out/bin/porn-vault" \
-      --chdir "$out/share/porn-vault" \
-      --add-flags "dist/index.js" \
-      --set-default IZZY_PATH "${lib.getExe izzy}" \
+    makeWrapper "$out/bin/porn-vault-unwrapped" "$out/bin/porn-vault" \
       --prefix PATH : "${
         lib.makeBinPath [
           ffmpeg
           imagemagick
-          izzy
+          openssl
         ]
       }"
   '';
@@ -110,7 +94,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     homepage = "https://gitlab.com/porn-vault/porn-vault";
     license = lib.licenses.gpl3Plus;
     maintainers = [ lib.maintainers.luNeder ];
-    inherit (nodejs-slim.meta) platforms;
+    inherit (nodejs.meta) platforms;
     mainProgram = "porn-vault";
   };
 })
